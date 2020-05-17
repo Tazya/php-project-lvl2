3a70de24451be5420d0d962e798650acc5b5f15b
Tazya <vengador32@gmail.com>
2020-05-17 20:12:35 +0600

    refactoring

--- src/Formatters/pretty.php
+++ src/Formatters/pretty.php
@@ -7,18 +7,18 @@ function normalizeValue($rawValue, $depth)
     $iterFormatArray = function ($array, $depth) use (&$iterFormatArray) {
         $indentStrInn = makeIndent($depth + 1);
         $indentStrOuter = makeIndent($depth);
+
         $formattedArray = array_map(function ($value, $key) use (&$iterFormatArray, $indentStrInn, $depth) {
             if (is_array($value)) {
-                return "$indentStrInn$key: " . $iterFormatArray($value, $depth + 1);
+                return "{$indentStrInn}{$key}: " . $iterFormatArray($value, $depth + 1);
             } else {
-                return "$indentStrInn$key: $value";
+                return "{$indentStrInn}{$key}: $value";
             }
         }, $array, array_keys($array));
 
         $formattedStr = implode("\n", $formattedArray);
         return "{\n$formattedStr\n$indentStrOuter}";
     };
-
 
     if (is_bool($rawValue)) {
         $normalizedValue = $rawValue ? 'true' : 'false';
@@ -36,7 +36,7 @@ function makeIndent($depth, $offset = 0)
     $indent = 4;
     $spacesCount = $indent * $depth + $offset;
     if ($spacesCount < 0) {
-        throw new \Exception("Indent cannot be less than 0\n");
+        throw new \Exception("Indent cannot be less than 0");
     }
     return implode("", array_fill(0, $spacesCount, " "));
 }
@@ -45,38 +45,33 @@ function renderPrettyDiff($ast)
 {
     $iter = function ($ast, $depth = 1) use (&$iter) {
         $diffs = array_map(function ($elem) use (&$iter, $depth) {
-            $indentStr = makeIndent($depth);
-            if (isset($elem['children'])) {
-                return $indentStr . $elem['name'] . ": " . $iter($elem['children'], $depth + 1);
-            }
+            $name = $elem['name'];
+            $value = isset($elem['value']) ? normalizeValue($elem['value'], $depth) : null;
+            $oldValue = isset($elem['oldValue']) ? normalizeValue($elem['oldValue'], $depth) : null;
+            $indentStr = makeIndent($depth, -2);
 
             switch ($elem['type']) {
+                case 'parent':
+                    $prefix = '  ';
+                    return "{$indentStr}  {$name}: " . $iter($elem['children'], $depth + 1);
                 case 'unchanged':
                     $prefix = '  ';
+                    $result = "$indentStr  $name: $value";
                     break;
                 case 'deleted':
                     $prefix = '- ';
+                    $result = "$indentStr- $name: $value";
                     break;
                 case 'added':
-                    $prefix = '+ ';
+                    $result = "$indentStr+ $name: $value";
                     break;
                 case 'changed':
+                    $result = "$indentStr+ $name: $value\n$indentStr- $name: $oldValue";
                     break;
                 default:
                     $unknownType = $elem['type'];
                     throw new \Exception("Difference type: '$unknownType' not found!\n");
                     break;
-            }
-
-            $name = $elem['name'];
-            $value = normalizeValue($elem['value'], $depth);
-
-            $indentStr2 = makeIndent($depth, -2);
-            if ($elem['type'] === 'changed') {
-                $oldValue = normalizeValue($elem['oldValue'], $depth);
-                $result = "$indentStr2+ $name: $value\n$indentStr2- $name: $oldValue";
-            } else {
-                $result = "$indentStr2$prefix$name: $value";
             }
 
             return $result;
